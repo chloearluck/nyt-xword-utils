d8411f01011c54cd43deb864047f1bb9cd5f618e
Chloe Arluck <chloearluck@gmail.com>
2020-06-14 13:36:22 -0400

    Extension works for acrostic puzzles as well

--- xwordutils.js
+++ xwordutils.js
@@ -2,17 +2,57 @@ const modifier = "Alt";
 const togglePencilKey = "KeyT"
 const toggleMenuKey = "KeyH"
 
-const menuBar = document.getElementsByClassName("Header-header--2X8Zj")[0];
-const pencilButton = document.getElementsByClassName("Icon-pencil--1cTxu Icon-icon--1RAWC")[0].parentElement;
+const getPencilButton = () => {
+  let pencilIcons = document.getElementsByClassName("Icon-pencil--1cTxu Icon-icon--1RAWC");
+  if (pencilIcons.length == 1)
+    return pencilIcons[0].parentElement;
+  pencilIcons = document.getElementsByClassName("pencil-mode-toggle")
+  if (pencilIcons.length == 1)
+    return pencilIcons[0];
+  return null;
+}
+
+const getMenuBar = () => {
+  let headers = document.getElementsByClassName("Header-header--2X8Zj");
+  if (headers.length == 1) {
+    return headers[0];
+  }
+  headers = document.getElementsByClassName("pz-header");
+  if (headers.length == 1) {
+    return headers[0];
+  }
+  return null;
+}
+
+let menuBar = document.getElementsByClassName("Header-header--2X8Zj")[0];
+let pencilButton = getPencilButton();
 
 let modifierDown = false;
+let display = "";
 
 const toggleMenu = () => {
-  menuBar.hidden = !menuBar.hidden;
+  if (!menuBar) {
+    menuBar = getMenuBar();
+  }
+
+  if (menuBar) {
+    if (!menuBar.hidden) {
+      display = menuBar.style.display;
+      menuBar.style.display = "none";
+      menuBar.hidden = true;
+    } else {
+      menuBar.hidden = true;
+      menuBar.style.display = display;
+    }
+  }
 }
 
 const togglePencil = () => {
-  pencilButton.click();
+  if (!pencilButton)
+    pencilButton = getPencilButton();
+
+  if (pencilButton)
+    pencilButton.click();
 }
 
 window.onkeydown = (event) => {
